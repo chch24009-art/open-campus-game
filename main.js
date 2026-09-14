c543342151d5c3ac9975269e35cf7fccf1e53670
chch24009-art <chch24009@g.nihon-u.ac.jp>
2026-09-14 16:35:07 +0900

    Format input element for attack name

--- main.js
+++ main.js
@@ -200,12 +200,12 @@ ui.innerHTML = '\
 \
 <div id="inputScreen" class="screen" style="display:none">\
     <h2>攻撃名を入力してください</h2>\
-    <input id="attackInput"
-       name="attack_name_no_save"
-       placeholder="例：ファイヤーブレイク"
-       maxlength="20"
-       autocomplete="new-password"
-       data-lpignore="true"
+    <input id="attackInput"\
+       name="attack_name_no_save"\
+       placeholder="例：ファイヤーブレイク"\
+       maxlength="20"\
+       autocomplete="new-password"\
+       data-lpignore="true"\
        data-form-type="other">\
     <button id="decideBtn">決定</button>\
 </div>\
